5fb939cd6d556b30bc8013e9580667695acef640
Ryan Bena <94004749+ryanbena@users.noreply.github.com>
2025-04-23 16:27:06 -0700

    Add files via upload

---
+++ PoissonTRO/inc/poisson.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define IMAX 120 // Grid X Size
+#define JMAX 120 // Grid Y Size
+#define TMAX 11 // Grid T Size
+#define DS 0.0294f // X-Y Grid Resolution
+#define DT 0.1 // MPC Step Size
+#define WN 0.8 // CBF Value
